842d8714cc0e858d2ba9dfcc62838743eabf9f8a
Dijalma <dijalma@cloudwalk.io>
2022-07-22 00:12:37 -0300

    Init Files

---
+++ packages/my-react-package/src/index.ts
@@ -0,0 +1,2 @@
+export { default as Hello } from './hello'; // export the default export from './hello', this is Hello component
+export * from './hello'; // export all named exports from './hello' like HelloProps
